03198e834ab9498702ee96741174ac302f75ce46
Deexit Kumar Sharma <deexitkumarsharma@gmail.com>
2022-12-19 16:57:27 +0530

    added all functionalities

--- script.js
+++ script.js
@@ -1,40 +1,85 @@
-function clearErrors() {
-  errors = document.getElementsByClassName("formerror");
-  for (let item of errors) {
-    item.innerHTML = "";
+// getting the inputs from document
+let nameInput = document.getElementById("name-id");
+let mail = document.getElementById("email-id");
+let love = document.getElementById("love-id");
+let color = document.getElementById("color-id");
+let rating = document.getElementById("range-id");
+
+let form = document.getElementById("form");
+let radio = document.getElementById("genre-div-id");
+let radioValue = "";
+
+let terms = document.querySelector("#check-box");
+
+radio.addEventListener("click", (event) => {
+  radioValue = event.target.value;
+});
+
+let array = [nameInput, mail];
+// adding elements to the modal
+
+let finalDiv = document.getElementById("modal");
+finalDiv.classList.add("form-results");
+let div = document.createElement("div");
+div.className = "forms-results";
+let closeDiv = document.createElement("div");
+
+let close = document.createElement("div");
+close.className = "close";
+close.textContent = "close";
+closeDiv.appendChild(close);
+
+let nameP = document.createElement("div");
+let mailP = document.createElement("div");
+let loveP = document.createElement("div");
+let colorP = document.createElement("div");
+let ratingP = document.createElement("div");
+let genreP = document.createElement("div");
+let termsP = document.createElement("div");
+termsP.className = "terms";
+
+div.appendChild(nameP);
+nameP.classList.add("classnameP");
+div.appendChild(mailP);
+div.appendChild(loveP);
+div.appendChild(colorP);
+div.appendChild(ratingP);
+div.appendChild(genreP);
+div.appendChild(termsP);
+
+finalDiv.appendChild(closeDiv);
+finalDiv.appendChild(div);
+finalDiv.style.display = "none";
+
+//adding event listener to form
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //for preventing from submitting empty data
+
+  for (let item of array) {
+    if (item.value === "") {
+      alert(`All details are mandotory`);
+      return false;
+    }
   }
-}
 
-function seterror(id, error) {
-  //sets error inside tag of id
-  element = document.getElementById(id);
-  element.getElementsByClassName("formerror")[0].innerHTML = error;
-}
+  form.style.display = "none"; //Important concept for popup
+  nameP.textContent = `Hello ${nameInput.value}`;
+  mailP.textContentP = `Email : ${mail.value}`;
+  loveP.textContent = `You Love : ${love.value}`;
+  colorP.textContent = `Color : ${color.value}`;
+  ratingP.textContent = `Rating : ${rating.value / 10}`;
+  genreP.textContent = `Book Genre : ${radioValue}`;
+  termsP.textContent = "👉 I Agreed to terms and conditions";
 
-function validateForm() {
-  var returnval = true;
-  clearErrors();
+  finalDiv.style.display = "flex";
 
-  //perform validation and if validation fails, set the value of returnval to false
-  var name = document.forms["myForm"]["fname"].value;
-  if (name.length < 5) {
-    seterror("name", "*Length of name is too short");
-    returnval = false;
-  }
-
-  if (name.length == 0) {
-    seterror("name", "*Length of name cannot be zero!");
-    returnval = false;
-  }
-
-  var email = document.forms["myForm"]["femail"].value;
-  if (email.length > 15) {
-    seterror("email", "*Email length is too long");
-    returnval = false;
-  }
-
-  return returnval;
-}
-
-
-
+  close.addEventListener("click", () => {
+    console.log("gone");
+    finalDiv.style.display = "none";
+    form.style.display = "flex";
+  });
+  form.reset();
+});
